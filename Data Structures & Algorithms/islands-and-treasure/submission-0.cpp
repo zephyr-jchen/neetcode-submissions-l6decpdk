0f40a24b83e6926dcd399cec89f9b148ed8a5e63
zephyr-jchen <jche2656@uwo.ca>
2026-05-13 22:26:42 +0800

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int dic[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+        queue<pair<int,int>> pq;
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == 0){
+                    pq.push({i,j});
+                }
+            }
+        }
+        while(!pq.empty()){
+            pair<int,int> p = pq.front();
+            pq.pop(); 
+            int r = p.first;
+            int c = p.second;
+            for(int i = 0; i < 4; i++){
+                int nr = r + dic[i][0];
+                int nc = c + dic[i][1];
+                if(nr>=0&&nc>=0&&nr<rows&&nc<cols&&grid[nr][nc]==2147483647){
+                    grid[nr][nc] = grid[r][c] + 1;
+                    pq.push({nr,nc});
+                }
+                
+            }
+            
+
+        }
+    }
+
+};
